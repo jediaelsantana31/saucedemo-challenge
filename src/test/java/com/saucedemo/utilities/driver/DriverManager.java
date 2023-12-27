fe861c5cd17cd864c5daefc251e37b2a68800af3
jediaelsantana31 <jediael.santana@gmail.com>
2023-12-27 17:04:53 -0300

    atualiza adiciona mais uma opcao de browser

--- src/test/java/com/saucedemo/utilities/driver/DriverManager.java
+++ src/test/java/com/saucedemo/utilities/driver/DriverManager.java
@@ -9,8 +9,21 @@ public class DriverManager {
     private WebDriver driver;
 
     public void initializeDriver() {
-        System.setProperty(ProjectSettings.CHROME_DRIVER, ProjectSettings.CHROMEDRIVER_PATH);
         ChromeOptions options = new ChromeOptions();
+
+        // Verifica se a variável de sistema está presente e configurada como "true"
+        String headlessMode = System.getProperty("headlessMode");
+        boolean isHeadless = "true".equalsIgnoreCase(headlessMode);
+
+        // Configuração para headless se a variável estiver presente e configurada como "true"
+        if (isHeadless) {
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--headless");
+        } else {
+            System.setProperty(ProjectSettings.CHROME_DRIVER, ProjectSettings.CHROMEDRIVER_PATH);
+        }
+
         driver = new ChromeDriver(options);
     }
 
