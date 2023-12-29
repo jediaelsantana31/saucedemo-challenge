d410dbd3c3f5a566e95831d3cfcbeed13e2732d2
jediaelsantana31 <jediael.santana@gmail.com>
2023-12-29 08:22:24 -0300

    update

--- src/test/java/com/saucedemo/steps/Hooks.java
+++ src/test/java/com/saucedemo/steps/Hooks.java
@@ -1,16 +1,9 @@
 package com.saucedemo.steps;
 
-import com.google.common.collect.ImmutableMap;
-import com.saucedemo.utilities.base.SeleniumHelper;
-import com.saucedemo.utilities.settings.ProjectSettings;
 import com.saucedemo.utilities.settings.ScenarioContext;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
-import org.openqa.selenium.Capabilities;
-import org.openqa.selenium.remote.DesiredCapabilities;
-
-//import static com.github.automatedowl.tools.AllureEnvironmentWriter.allureEnvironmentWriter;
 
 public class Hooks {
     private final ScenarioContext scenarioContext = ScenarioContext.getInstance();
@@ -19,17 +12,6 @@ public class Hooks {
     @Before
     public void beforeScenario() {
         scenarioContext.getDriver();
-        configureReport();
-    }
-
-    public void configureReport() {
-        Capabilities capabilities = scenarioContext.printBrowserInfo();
-        /*allureEnvironmentWriter(
-                ImmutableMap.<String, String>builder()
-                        .put("Browser", capabilities.getBrowserName())
-                        .put("Browser.Version", capabilities.getBrowserVersion())
-                        .put("URL", ProjectSettings.URL).build(),
-                System.getProperty("user.dir") + "/allure-results/");*/
     }
 
     @After
